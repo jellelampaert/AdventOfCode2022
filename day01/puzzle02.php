c2fce572537d20a6a9d203004bd2dea2b7c9a84e
Jelle Lampaert <info@jellelampaert.be>
2022-12-07 22:59:32 +0100

    Day 01

---
+++ day01/puzzle02.php
@@ -0,0 +1,22 @@
+<?php
+
+$content = file('input.txt');
+
+$calories = array();
+$index = 0;
+
+// Initialize first value
+$calories[0] = 0;
+foreach ($content as $line) {
+    if (trim($line) == "") {
+        $index++;
+        $calories[$index] = 0;
+    } else {
+        $cals = (int)trim($line);
+        $calories[$index] += $cals;
+    }
+}
+
+rsort($calories);
+echo ($calories[0] + $calories[1] + $calories[2]);
+echo "\r\n";
